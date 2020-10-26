e2749d6e09bd50943ae0472f20854bcbd2d39bd1
NoisyApple <leonel.aguirre77@gmail.com>
2020-10-26 17:09:57 -0600

    MainMenu layout done.

--- src/main/java/com/noisyapple/App.java
+++ src/main/java/com/noisyapple/App.java
@@ -1,52 +1,53 @@
 package com.noisyapple;
 
-import java.util.Arrays;
-import java.util.Stack;
-
 public class App {
 
     public static void main(String[] args) {
 
-        String[] cases = {"10", "010110", "", "01"};
+        new MainMenu();
 
-        String alphabet = "01"; // Sigma.
+        // String[] cases = {"10", "010110", "", "01"};
 
-        String[] Q1 = {"i", "0", "1", "00", "01", "10", "11"}; // Q.
-        String q0_1 = "i"; // q0.
-        Transition[] trans_1 = {new Transition("i", '0', "0"), new Transition("i", '1', "1"),
-                new Transition("0", '0', "00"), new Transition("0", '1', "01"),
-                new Transition("1", '0', "10"), new Transition("1", '1', "11"),
-                new Transition("00", '0', "00"), new Transition("00", '1', "01"),
-                new Transition("01", '0', "10"), new Transition("01", '1', "11"),
-                new Transition("10", '0', "00"), new Transition("10", '1', "01"),
-                new Transition("11", '0', "10"), new Transition("11", '1', "11")}; // Delta.
-        String[] F1 = {"10"}; // F.
+        // String alphabet = "01"; // Sigma.
 
-        DFA M1 = new DFA(Q1, alphabet, q0_1, trans_1, F1); // (0+1)*10
+        // String[] Q1 = {"i", "0", "1", "00", "01", "10", "11"}; // Q.
+        // String q0_1 = "i"; // q0.
+        // Transition[] trans_1 = {new Transition("i", '0', "0"), new Transition("i", '1', "1"),
+        // new Transition("0", '0', "00"), new Transition("0", '1', "01"),
+        // new Transition("1", '0', "10"), new Transition("1", '1', "11"),
+        // new Transition("00", '0', "00"), new Transition("00", '1', "01"),
+        // new Transition("01", '0', "10"), new Transition("01", '1', "11"),
+        // new Transition("10", '0', "00"), new Transition("10", '1', "01"),
+        // new Transition("11", '0', "10"), new Transition("11", '1', "11")}; // Delta.
+        // String[] F1 = {"10"}; // F.
 
-        String[] Q2 = {"A", "B", "C"}; // Q.
-        String q0_2 = "A"; // q0.
-        Transition[] trans_2 = {new Transition("A", '0', "A"), new Transition("A", '1', "B"),
-                new Transition("B", '1', "B"), new Transition("B", '0', "C"),
-                new Transition("C", '1', "B"), new Transition("C", '0', "A"),}; // Delta.
-        String[] F2 = {"C"}; // F.
+        // DFA M1 = new DFA(Q1, alphabet, q0_1, trans_1, F1); // (0+1)*10
 
-        DFA M2 = new DFA(Q2, alphabet, q0_2, trans_2, F2); // (0+1)*10
+        // String[] Q2 = {"A", "B", "C"}; // Q.
+        // String q0_2 = "A"; // q0.
+        // Transition[] trans_2 = {new Transition("A", '0', "A"), new Transition("A", '1', "B"),
+        // new Transition("B", '1', "B"), new Transition("B", '0', "C"),
+        // new Transition("C", '1', "B"), new Transition("C", '0', "A"),}; // Delta.
+        // String[] F2 = {"C"}; // F.
 
-        System.out.println(M1.toString());
-        System.out.println(M2.toString());
+        // DFA M2 = new DFA(Q2, alphabet, q0_2, trans_2, F2); // (0+1)*10
 
-        for (String c : cases) {
-            System.out.println("[TEST SEQUENCE] => \"" + c + "\"");
-            System.out.println("M1 => " + M1.evaluate(c));
-            System.out.println("M2 => " + M2.evaluate(c) + "\n");
-        }
+        // System.out.println(M1.toString());
+        // System.out.println(M2.toString());
 
-        Stack<StateTuple[]> data = DFA.areEquivalent(M1, M2);
+        // for (String c : cases) {
+        // System.out.println("[TEST SEQUENCE] => \"" + c + "\"");
+        // System.out.println("M1 => " + M1.evaluate(c));
+        // System.out.println("M2 => " + M2.evaluate(c) + "\n");
+        // }
 
-        for (StateTuple[] row : data) {
-            System.out.println(Arrays.toString(row));
-        }
+        // Stack<StateTuple[]> data = DFA.areEquivalent(M1, M2);
+
+        // for (StateTuple[] row : data) {
+        // System.out.println(Arrays.toString(row));
+        // }
+
+        ////////////////////////////////////
 
         // System.out.println("\n\n");
 
---
+++ src/main/java/com/noisyapple/MainMenu.java
@@ -0,0 +1,121 @@
+package com.noisyapple;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSeparator;
+import javax.swing.border.EmptyBorder;
+
+@SuppressWarnings("serial")
+public class MainMenu extends JFrame {
+
+    private JPanel mainPanel, alphabetPanel, m1Panel, m2Panel, m1StatesPanel, m2StatesPanel,
+            m1TransitionsPanel, m2TransitionsPanel, bottomPanel;
+    private JButton btnAlphabet, btnM1States, btnM2States, btnM1Transitions, btnM2Transitions,
+            btnCompare, btnReset;
+    private JLabel lblAlphabet, lblM1States, lblM2States, lblM1Transitions, lblM2Transitions;
+
+    public MainMenu() {
+
+        mainPanel = new JPanel(new BorderLayout(10, 10));
+
+        alphabetPanel = new JPanel();
+        btnAlphabet = new JButton("ALPHABET");
+        lblAlphabet = new JLabel("No alphabet set.");
+
+        m1Panel = new JPanel();
+        m2Panel = new JPanel();
+
+        m1StatesPanel = new JPanel();
+        btnM1States = new JButton("STATES");
+        lblM1States = new JLabel("No states set.");
+
+        m2StatesPanel = new JPanel();
+        btnM2States = new JButton("STATES");
+        lblM2States = new JLabel("No states set.");
+
+        m1TransitionsPanel = new JPanel();
+        btnM1Transitions = new JButton("TRANSITIONS");
+        lblM1Transitions = new JLabel("No transitions set.");
+
+        m2TransitionsPanel = new JPanel();
+        btnM2Transitions = new JButton("TRANSITIONS");
+        lblM2Transitions = new JLabel("No transitions set.");
+
+        bottomPanel = new JPanel();
+        btnCompare = new JButton("COMPARE");
+        btnReset = new JButton("RESET");
+
+        addAttributes();
+        build();
+        launch();
+
+    }
+
+    public void addAttributes() {
+        setTitle("Automaton equivalence");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        alphabetPanel.setBorder(BorderFactory.createTitledBorder("Alphabet"));
+
+        m1Panel.setBorder(BorderFactory.createTitledBorder("M1"));
+        m2Panel.setBorder(BorderFactory.createTitledBorder("M2"));
+
+        m1Panel.setLayout(new BoxLayout(m1Panel, BoxLayout.Y_AXIS));
+        m2Panel.setLayout(new BoxLayout(m2Panel, BoxLayout.Y_AXIS));
+
+        btnM1States.setPreferredSize(new Dimension(111, 26));
+        lblM1States.setPreferredSize(new Dimension(103, 26));
+        btnM2States.setPreferredSize(new Dimension(111, 26));
+        lblM2States.setPreferredSize(new Dimension(103, 26));
+
+        mainPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+
+    }
+
+    public void build() {
+        alphabetPanel.add(btnAlphabet);
+        alphabetPanel.add(lblAlphabet);
+
+        m1StatesPanel.add(btnM1States);
+        m1StatesPanel.add(lblM1States);
+
+        m2StatesPanel.add(btnM2States);
+        m2StatesPanel.add(lblM2States);
+
+        m1TransitionsPanel.add(btnM1Transitions);
+        m1TransitionsPanel.add(lblM1Transitions);
+
+        m2TransitionsPanel.add(btnM2Transitions);
+        m2TransitionsPanel.add(lblM2Transitions);
+
+        bottomPanel.add(btnCompare);
+        bottomPanel.add(btnReset);
+
+        m1Panel.add(m1StatesPanel);
+        m2Panel.add(m2StatesPanel);
+
+        m1Panel.add(m1TransitionsPanel);
+        m2Panel.add(m2TransitionsPanel);
+
+        mainPanel.add(alphabetPanel, BorderLayout.NORTH);
+        mainPanel.add(m1Panel, BorderLayout.WEST);
+        mainPanel.add(new JSeparator(JSeparator.VERTICAL), BorderLayout.CENTER);
+        mainPanel.add(m2Panel, BorderLayout.EAST);
+        mainPanel.add(bottomPanel, BorderLayout.SOUTH);
+
+        add(mainPanel);
+    }
+
+    public void launch() {
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+}
